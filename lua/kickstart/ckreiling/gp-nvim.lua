7f20c2b5305dc401454650699db47112977d54a7
Christian Kreiling <kreiling@hey.com>
2024-07-24 10:54:28 -0400

    More versatile llm config

--- lua/kickstart/ckreiling/gp-nvim.lua
+++ lua/kickstart/ckreiling/gp-nvim.lua
@@ -2,6 +2,24 @@ local function lookup(table, key, default)
   return table[key] ~= nil and table[key] or default
 end
 
+local function read_and_decode_json(file_path)
+  if not vim.fn.filereadable(file_path) then
+    return nil
+  end
+
+  local ok, contents_list = pcall(vim.fn.readfile, file_path)
+  if not ok then
+    return nil
+  end
+
+  local ok, decoded = pcall(vim.json.decode, table.concat(contents_list, '\n'))
+  if not ok then
+    return nil
+  end
+
+  return decoded
+end
+
 return function(config)
   local disable_anthropic = lookup(config, 'disable_anthropic', false)
   local disable_copilot = lookup(config, 'disable_copilot', false)
@@ -11,6 +29,26 @@ return function(config)
   return {
     'robitx/gp.nvim',
     config = function()
+      -- This is so ugly...
+      local copilot = { config = nil, secret = nil }
+      if not disable_copilot then
+        copilot['config'] = read_and_decode_json(os.getenv 'HOME' .. '/.config/github-copilot/hosts.json')
+        if copilot['config'] then
+          local cfg = copilot['config']
+          copilot['secret'] = cfg and cfg['github.com'] and cfg['github.com']['oauth_token'] or nil
+          if not copilot['secret'] then
+            vim.api.nvim_err_writeln 'Warning: GitHub Copilot secret not found in CoPilot config (will not use Copilot for Gp* commands)'
+          end
+        else
+          vim.api.nvim_err_writeln 'Warning: GitHub Copilot config not found (will not use Copilot for Gp* commands)'
+        end
+      end
+
+      local anthropic_token = os.getenv 'ANTHROPIC_API_KEY'
+      if not disable_anthropic and not anthropic_token then
+        vim.api.nvim_err_writeln 'Warning: ANTHROPIC_API_KEY not found (will not use Anthropic for Gp* commands)'
+      end
+
       local conf = {
         openai_api_key = 'bogus', -- must be set to avoid errors
         providers = {
@@ -18,18 +56,14 @@ return function(config)
             disable = true,
           },
           anthropic = {
-            disable = disable_anthropic,
+            disable = anthropic_token == nil or disable_anthropic,
             endpoint = 'https://api.anthropic.com/v1/messages',
-            secret = os.getenv 'ANTHROPIC_API_KEY',
+            secret = anthropic_token or 'unknown',
           },
           copilot = {
-            disable = disable_copilot,
+            disable = copilot['secret'] == nil or disable_copilot,
             endpoint = 'https://api.githubcopilot.com/chat/completions',
-            secret = {
-              'bash',
-              '-c',
-              'jq -r \'.["github.com"].oauth_token\' ~/.config/github-copilot/hosts.json',
-            },
+            secret = copilot['secret'] or 'unknown',
           },
         },
         agents = {
