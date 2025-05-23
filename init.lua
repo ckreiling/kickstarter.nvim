a0bb34ab3d47d218f8e5c49d521fb1a3bd595078
Christian Kreiling <kreiling@hey.com>
2025-05-23 17:30:26 -0400

    Replace custom tab navigation with Bufferline keybindings and enable diagnostics

--- init.lua
+++ init.lua
@@ -198,10 +198,6 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- Keybinds to make tab navigation easier.
-vim.keymap.set('n', '<Tab>', '<cmd>bnext<CR>', { desc = 'Go to next buffer' })
-vim.keymap.set('n', '<S-Tab>', '<cmd>bprevious<CR>', { desc = 'Go to previous buffer' })
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- lua/kickstart/ckreiling/bufferline.lua
+++ lua/kickstart/ckreiling/bufferline.lua
@@ -9,7 +9,18 @@ return {
     require('bufferline').setup {
       options = {
         separator_style = 'slant',
+        diagnostics = 'nvim_lsp',
+        diagnostics_indicator = function(count, level)
+          local icon = level:match 'error' and ' ' or ' '
+          return ' ' .. icon .. count
+        end,
+        show_close_icon = true,
+        show_buffer_close_icons = true,
       },
     }
+
+    -- Keybindings for buffer navigation
+    vim.api.nvim_set_keymap('n', '<Tab>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<S-Tab>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
   end,
 }
