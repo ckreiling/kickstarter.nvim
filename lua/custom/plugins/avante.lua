d19d830cd0f4128d4b923e1030449f854b0b400f
Christian Kreiling <kreiling@hey.com>
2025-05-31 18:51:58 -0400

    Fix Avante bug with root directory detection and re-enable system_prompt functionality.

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -4,12 +4,18 @@ return {
   event = 'VeryLazy',
   version = false,
   config = function()
+    -- There is an Avante bug where it tries to read the current buffer from the
+    -- git root, instead of the current working directory according to Neovim.
+    -- Admittedly this is blindly pasted from a GitHub issue, but it seems to
+    -- work: https://github.com/yetone/avante.nvim/issues/2046#issuecomment-2905410720
+    vim.g.root_spec = { { '.git' }, 'lsp', 'cwd' }
+
     require('avante').setup {
       provider = os.getenv 'ANTHROPIC_API_KEY' and 'claude' or 'copilot',
-      -- system_prompt = function()
-      --   local hub = require('mcphub').get_hub_instance()
-      --   return hub and hub:get_active_servers_prompt() or ''
-      -- end,
+      system_prompt = function()
+        local hub = require('mcphub').get_hub_instance()
+        return hub and hub:get_active_servers_prompt() or ''
+      end,
       claude = {
         model = os.getenv 'ANTHROPIC_MODEL' or 'claude-3-7-sonnet-20250219',
       },
