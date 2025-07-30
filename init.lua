6e6db48aa27aec471099ea8665b2c72c68e7563e
Christian Kreiling <kreiling@hey.com>
2025-07-30 14:13:11 -0600

    Ugh does not work but whatever

--- init.lua
+++ init.lua
@@ -1041,10 +1041,9 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- TODO: Make this dynamic somehow.
+      -- For some reason, when Ghostty updates the background it doesn't always work.
+      vim.cmd.colorscheme(vim.o.background == 'light' and 'gruvbox' or 'tokyonight-night')
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
