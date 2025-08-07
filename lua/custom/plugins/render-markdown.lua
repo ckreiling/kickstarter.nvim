81c525c450ddd89408f8fde038b7e4d29368bb13
Christian Kreiling <kreiling@hey.com>
2025-08-07 07:27:28 -0600

    Tweak render-markdown settings

--- lua/custom/plugins/render-markdown.lua
+++ lua/custom/plugins/render-markdown.lua
@@ -1,8 +1,14 @@
 ---@type LazyPluginSpec
 return {
   'MeanderingProgrammer/render-markdown.nvim',
+  ---@type render.md.UserConfig
   opts = {
     file_types = { 'markdown', 'Avante', 'codecompanion' },
+    heading = {
+      position = 'overlay',
+      left_pad = 1,
+      right_pad = 1,
+    },
   },
   ft = { 'markdown', 'codecompanion', 'Avante' },
 }
